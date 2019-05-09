d7bd0cd5fe272dfed624a2b8bf3068645785d029
NarayanSchuetz <narayan.schuetz@artorg.unibe.ch>
2019-05-09 19:04:44 +0200

    Add weight normalization and random init

--- spectral/layers2d.py
+++ spectral/layers2d.py
@@ -16,7 +16,7 @@ from .util import build_base_matrix_1d
 
 class Spectral2dBase(nn.Module):
 
-    def __init__(self, nrows, ncols, fixed, base_matrix_builder=None):
+    def __init__(self, nrows, ncols, fixed, base_matrix_builder=None, weight_normalization=False, scaling_factor=1):
         """
         :param nrows: the number of rows of the 2d input (y dimension)
         :type nrows: int
@@ -31,10 +31,12 @@ class Spectral2dBase(nn.Module):
         self.requires_grad = not fixed
         self.base_matrix_builder = base_matrix_builder
         self.register_parameter('bias', None)
+        self._weight_normalization = weight_normalization
+        self._scaling_factor = scaling_factor
 
     def extra_repr(self):
         return 'nrows_in={}, ncols_in={}, bias=False'.format(
-            self.x_dim, self.y_dim
+            self.nrows, self.ncols
         )
 
     def forward(self, *input):
@@ -48,8 +50,20 @@ class Dft2d(Spectral2dBase):
     x-axis but double the y-axis of the input -> input: n_x, n_y, output: n_x, 2 x n_y
     """
 
-    def __init__(self, nrows, ncols, fixed=False, base_matrix_builder=build_base_matrix_1d, mode='amp', redundance = True):
-        super().__init__(nrows, ncols, fixed, base_matrix_builder)
+    def __init__(
+            self,
+            nrows,
+            ncols,
+            fixed=False,
+            base_matrix_builder=build_base_matrix_1d,
+            mode='amp',
+            redundance=True,
+            random_init=False,
+            weight_normalization=False,
+            scaling_factor=1
+    ):
+
+        super().__init__(nrows, ncols, fixed, base_matrix_builder, weight_normalization=weight_normalization, scaling_factor=scaling_factor)
 
         self.mode = mode
         self.redundance = redundance
@@ -59,8 +73,12 @@ class Dft2d(Spectral2dBase):
         self._real = None
         self._imag = None
 
-        real_tensor1, imag_tensor1 = self._create_weight_tensors(signal_length=self.nrows)
-        real_tensor2, imag_tensor2 = self._create_weight_tensors(signal_length=self.ncols)
+        if random_init:
+            real_tensor1, imag_tensor1 = self._create_random_weight_tensors(signal_length=self.nrows)
+            real_tensor2, imag_tensor2 = self._create_random_weight_tensors(signal_length=self.ncols)
+        else:
+            real_tensor1, imag_tensor1 = self._create_weight_tensors(signal_length=self.nrows)
+            real_tensor2, imag_tensor2 = self._create_weight_tensors(signal_length=self.ncols)
 
         self.weights_real1 = nn.Parameter(real_tensor1, requires_grad=self.requires_grad)
         self.weights_real2 = nn.Parameter(real_tensor2, requires_grad=self.requires_grad)
@@ -69,10 +87,23 @@ class Dft2d(Spectral2dBase):
         self.weights_imag2 = nn.Parameter(imag_tensor2, requires_grad=self.requires_grad)
 
     def _create_weight_tensors(self, signal_length):
+
+        a = self._scaling_factor*np.sqrt(6/(2*signal_length)) if self._weight_normalization else 1
+
         X_base = self.base_matrix_builder(signal_length, redundance=self.redundance, forward=True)
         T_real = torch.tensor(np.cos(X_base), dtype=torch.float32)
+        T_real *= a
         T_imag = torch.tensor(np.sin(X_base), dtype=torch.float32)
+        T_imag *= a
+
         return T_real, T_imag
+
+    def _create_random_weight_tensors(self, signal_length):
+        X_1 = torch.empty(signal_length, signal_length)
+        X_2 = torch.empty(signal_length, signal_length)
+        nn.init.xavier_uniform_(X_1, self._scaling_factor)
+        nn.init.xavier_uniform_(X_2, self._scaling_factor)
+        return X_1, X_2
 
     def _create_amplitude_phase(self):
         self._amp = torch.sqrt(self._real ** 2 + self._imag ** 2)
@@ -107,17 +138,23 @@ class DctII2d(Spectral2dBase):
     where the transform will be applied to - as is usually the case in PyTorch.
     """
 
-    def __init__(self, nrows, ncols, fixed=False):
-        super().__init__(nrows, ncols, fixed)
+    def __init__(self, nrows, ncols, fixed=False, random_init=False, scaling_factor=1, weight_normalization=False):
+        super().__init__(nrows, ncols, fixed, weight_normalization=weight_normalization, scaling_factor=scaling_factor)
 
-        self.weights_1 = nn.Parameter(self._create_weight_tensor(self.nrows), requires_grad=self.requires_grad)
-        self.weights_2 = nn.Parameter(self._create_weight_tensor(self.ncols), requires_grad=self.requires_grad)
+        if random_init:
+            self.weights_1 = nn.Parameter(self._create_random_weight_tensor(self.nrows), requires_grad=self.requires_grad)
+            self.weights_2 = nn.Parameter(self._create_random_weight_tensor(self.ncols), requires_grad=self.requires_grad)
+        else:
+            self.weights_1 = nn.Parameter(self._create_weight_tensor(self.nrows), requires_grad=self.requires_grad)
+            self.weights_2 = nn.Parameter(self._create_weight_tensor(self.ncols), requires_grad=self.requires_grad)
 
     def _create_weight_tensor(self, signal_length):
         """
         Generate matrix with coefficients of discrete cosine transformation
         Here, DCT II is implemented https://en.wikipedia.org/wiki/Discrete_cosine_transform#DCT-II
         """
+        a = self._scaling_factor*np.sqrt(6/(2*signal_length)) if self._weight_normalization else 1
+
         n = np.arange(0, signal_length, 1, dtype=np.float32) + 0.5
         X = np.asmatrix(np.tile(n, (signal_length, 1)))
 
@@ -129,7 +166,13 @@ class DctII2d(Spectral2dBase):
         X = X * (np.pi / signal_length)
 
         X_i = np.cos(X)
+        X_i *= a
         return torch.tensor(X_i, dtype=torch.float32)
+
+    def _create_random_weight_tensor(self, signal_length):
+        X = torch.empty(signal_length, signal_length)
+        nn.init.xavier_uniform_(X, self._scaling_factor)
+        return X
 
     def forward(self, input):
         x = F.linear(torch.transpose(input, -2, -1), self.weights_1)
@@ -144,9 +187,17 @@ class iDft2d(Spectral2dBase):
     where the transform will be applied to - as is usually the case in PyTorch.
     """
 
-    def __init__(self, nrows, ncols, fixed=False, mode='complex'):
-        super().__init__(nrows, ncols, fixed)
+    def __init__(
+            self,
+            nrows,
+            ncols,
+            fixed=False,
+            mode='complex',
+            random_init=False,
+            scaling_factor=1,
+            weight_normalization=False):
 
+        super().__init__(nrows, ncols, fixed, scaling_factor=scaling_factor, weight_normalization=weight_normalization)
         self.mode = mode
 
         self._amp = None
@@ -154,15 +205,23 @@ class iDft2d(Spectral2dBase):
         self._real = None
         self._imag = None
 
-        real_tensor_1, imag_tensor_1 = self._create_weight_tensors(self.nrows)
+        if random_init:
+            real_tensor_1, imag_tensor_1 = self._create_random_weight_tensors(self.nrows)
+        else:
+            real_tensor_1, imag_tensor_1 = self._create_weight_tensors(self.nrows)
         self.weights_real_1 = nn.Parameter(real_tensor_1, requires_grad=self.requires_grad)
         self.weights_imag_1 = nn.Parameter(imag_tensor_1, requires_grad=self.requires_grad)
 
-        real_tensor_2, imag_tensor_2 = self._create_weight_tensors(self.ncols)
+        if random_init:
+            real_tensor_2, imag_tensor_2 = self._create_random_weight_tensors(self.ncols)
+        else:
+            real_tensor_2, imag_tensor_2 = self._create_weight_tensors(self.ncols)
         self.weights_real_2 = nn.Parameter(real_tensor_2, requires_grad=self.requires_grad)
         self.weights_imag_2 = nn.Parameter(imag_tensor_2, requires_grad=self.requires_grad)
 
     def _create_weight_tensors(self, signal_length):
+
+        a = self._scaling_factor*np.sqrt(3*signal_length) if self._weight_normalization else 1
 
         n = np.arange(0, signal_length, 1, dtype=np.float32)
         X = np.asmatrix(np.tile(n, (signal_length, 1)))
@@ -173,9 +232,18 @@ class iDft2d(Spectral2dBase):
         X_r = 1 / np.sqrt((self.nrows * self.ncols)) * np.cos(X)
         X_i = 1 / np.sqrt((self.nrows * self.ncols)) * np.sin(X)
 
+        X_r *= a
+        X_i *= a
         return torch.tensor(X_r, dtype=torch.float32), torch.tensor(X_i, dtype=torch.float32)
 
-    def _create_complex(self, input, feat_num):
+    def _create_random_weight_tensors(self, signal_length):
+        X_1 = torch.empty(signal_length, signal_length)
+        X_2 = torch.empty(signal_length, signal_length)
+        nn.init.xavier_uniform_(X_1, self._scaling_factor)
+        nn.init.xavier_uniform_(X_2, self._scaling_factor)
+        return X_1, X_2
+
+    def _create_ampphase(self, input, feat_num):
         self._amp = input[:, :feat_num//2, :, :]
         self._phase = input[:, feat_num//2:, :, :]
 
@@ -189,7 +257,7 @@ class iDft2d(Spectral2dBase):
             raise  IndexError("dimension should be even - half real/amp and half imag/phase")
 
         if self.mode == 'amp':
-            self._create_complex(input, feat_num)
+            self._create_ampphase(input, feat_num)
         elif self.mode == 'complex':
             self._real = input[:, :feat_num//2, :, :]
             self._imag = input[:, feat_num//2:, :, :]
@@ -222,13 +290,18 @@ class iDctII2d(Spectral2dBase):
     where the transform will be applied to - as is usually the case in PyTorch.
     """
 
-    def __init__(self, nrows, ncols, fixed=False):
-        super().__init__(nrows, ncols, fixed)
+    def __init__(self, nrows, ncols, fixed=False, random_init=False, weight_normalization=False, scaling_factor=False):
+        super().__init__(nrows, ncols, fixed, weight_normalization=weight_normalization, scaling_factor=scaling_factor)
 
-        self.weights_1 = nn.Parameter(self._create_weight_tensor(self.nrows), requires_grad=self.requires_grad)
-        self.weights_2 = nn.Parameter(self._create_weight_tensor(self.ncols), requires_grad=self.requires_grad)
+        if random_init:
+            self.weights_1 = nn.Parameter(self._create_random_weight_tensor(self.nrows), requires_grad=self.requires_grad)
+            self.weights_2 = nn.Parameter(self._create_random_weight_tensor(self.ncols), requires_grad=self.requires_grad)
+        else:
+            self.weights_1 = nn.Parameter(self._create_weight_tensor(self.nrows), requires_grad=self.requires_grad)
+            self.weights_2 = nn.Parameter(self._create_weight_tensor(self.ncols), requires_grad=self.requires_grad)
 
     def _create_weight_tensor(self, signal_length):
+        a = self._scaling_factor * np.sqrt(3*signal_length)/2 if self._weight_normalization else 1
 
         n = np.arange(0, signal_length, 1, dtype=np.float32)
         X = np.asmatrix(np.tile(n, (signal_length, 1)))
@@ -240,8 +313,14 @@ class iDctII2d(Spectral2dBase):
 
         X[:, 0] = 0.5 * X[:, 0]
         X_i = (2 / signal_length) * X
+        X_i *= a
 
         return torch.tensor(X_i, dtype=torch.float32)
+
+    def _create_random_weight_tensor(self, signal_length):
+        X = torch.empty(signal_length, signal_length)
+        nn.init.xavier_uniform_(X, self._scaling_factor)
+        return X
 
     def forward(self, input):
         x = F.linear(torch.transpose(input, -2, -1), self.weights_1)
